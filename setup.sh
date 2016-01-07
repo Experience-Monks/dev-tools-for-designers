d7e37b31715085a06cab1c4b95941eb755bd047e
nascherman <n_scherman@hotmail.com>
2016-01-07 14:32:10 -0600

    Added the setup script

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#Download and install nodejs - requires password
+#curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+#Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#Use Homebrew to get latest node
+brew update
+export PATH="/usr/local/bin:$PATH"
+brew install node
+
+#Fixing npm permission
+if ["$(npm config get prefix)" -ne "/usr"]; then
+	sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+else
+	echo "Setting up local account."	
+	mkdir ~/.npm-global
+	npm config set prefix '~/.npm-global'
+	printf "export PATH=~/.npm-global/bin:$PATH" >> ~/.bash_profile
+	source ~/.bash_profile
+fi
+#Sublime installation - version 2.0.2
+mkdir /Volumes/mnt
+curl -O  http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
+
+if hdiutil  attach -mountpoint /Volumes/mnt Sublime%20Text%202.0.2.dmg; then
+	cp -r /Volumes/mnt/Sublime\ Text\ 2.app /Applications/
+
+	hdiutil detach /Volumes/mnt
+	rm Sublime%20Text%202.0.2.dmg
+
+	mkdir ~/bin
+	printf "export PATH=~/bin:$PATH" >> ~/.bash_profile
+	source ~/.bash_profile
+
+	ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+else
+	echo "Sublime mount failed"
+fi
+
+#Github Desktop install
+curl -O https://mac-installer.github.com/mac/GitHub%20Desktop%20216.zip
+unzip GitHub%20Desktop%20216.zip
+#cp "GitHub Desktop.app" /Applications/
+echo "Launching Github Installer"
+open "GitHub Desktop.app"
